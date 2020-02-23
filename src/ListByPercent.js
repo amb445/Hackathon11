9ba8804910281c45271c8647fbd68056e2e6f947
amb445 <58574662+amb445@users.noreply.github.com>
2020-02-22 17:38:09 -0800

    Update ListByPercent.js

--- src/ListByPercent.js
+++ src/ListByPercent.js
@@ -11,11 +11,18 @@ class    ListByPercent extends React.Component  {
     {
     super(props);     
     this.state =  { rows :  [
-        createData('College Name 1', 159, 6, 24, 4.0,1,1,1,1),
-        createData('College Name 2', 237, 9, 37, 4.3,1,1,2,2,2),
-        createData('College Name 3', 262, 16, 24, 6.0,3,3,3,3,4),
-        createData('College Name 4', 305, 3, 67, 4.3,5,5,5,5,6),
-        createData('College Name 5', 356, 16, 49, 3.9,8,8,8,8,8),
+        createData('MIT', 1900, 6, 24, 4.0,1,1,1,1),
+        createData('Alabama A & M University', 2700, 9, 37, 4.3,1,1,2,2,2),
+        createData('J F Drake State Technical College', 5500, 16, 24, 6.0,3,3,3,3,4),
+        createData('Gadsden State Community College', 7007, 3, 67, 4.3,5,5,5,5,6),
+        createData('Oakwood University', 300, 16, 49, 3.9,8,8,8,8,8),
+        
+        createData('Talladega College', 300, 6, 24, 4.0,1,1,1,1),
+        createData('Alabama State University', 237, 9, 37, 4.3,1,1,2,2,2),
+        createData('H Councill Trenholm State Technical College', 262, 16, 24, 6.0,3,3,3,3,4),
+        createData('Concordia College-Selma', 300, 3, 67, 4.3,5,5,5,5,6),
+        createData('Tuskegee University', 300, 16, 49, 3.9,8,8,8,8,8),
+         createData('Stanford University', 300, 16, 49, 3.9,8,8,8,8,8),
       ]}      
     
   }  
